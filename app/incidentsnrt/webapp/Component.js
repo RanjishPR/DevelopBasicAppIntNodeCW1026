ebed802dc299db960730706d608c79146e6fff25
RANJISH P R <r.p.r@sap.com>
2026-03-02 15:46:49 +0000

    Initial

---
+++ app/incidentsnrt/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns.incidentsnrt.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
